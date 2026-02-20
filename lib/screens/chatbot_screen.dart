920e1a3ed707d6b99afa24184cb1a63185460fe9
sadan saba <sadansba@gmail.com>
2026-02-20 03:31:21 +0300

    Finished app structure and bottom navigation

---
+++ lib/screens/chatbot_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class ChatbotScreen extends StatelessWidget {
+  const ChatbotScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Chatbot Screen",
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
